8f2a9fe1c70a45d53ffa9ec9615884df15f983ef
Ruslan Tyutin <rus@motionrus-2.local>
2018-07-09 23:08:34 +0300

    create slider

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react';
 
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
+
 import styled from 'styled-components';
+
+import Slider from './Slider';
+
 
 const Repair = styled.div`
 	color: #ffffff;
@@ -56,7 +60,7 @@ class Main extends React.Component {
 					</TextButton>
 				</Col>
 				<Col lg={6} lgOffset={1}>
-
+					<Slider />
 				</Col>
 			</Row>
 			)
